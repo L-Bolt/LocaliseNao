31cc9776c66e777bfe1b5de6e15e7bb42a0f3ba0
lex <lex.bolt@student.uva.nl>
2023-06-11 14:50:36 +0200

    reimplemented optimized dataset saving

--- model/UnityDataset.py
+++ model/UnityDataset.py
@@ -4,22 +4,41 @@ from torch.utils.data import Dataset
 from pysolotools.consumers import Solo
 from PIL import Image
 import itertools
+import json
 
 class CustomData(Dataset):
-    def __init__(self, data_dir, transform=None):
+    def __init__(self, data_dir, transform=None, save=False):
         self.datadir = data_dir
         self.dataset = Solo(data_path=data_dir)
-        self.dataset_iterator = list(itertools.islice(self.dataset.frames(), 1, None, 2))
         self.img_filename = "step1.camera.png"
         self.transform = transform
         self.captures = self.dataset.metadata.totalSequences
+        self.loaded = False
+
+        if os.path.isfile(f"Dataset_metadata{self.captures}.json"):
+            with open(f"Dataset_metadata{self.captures}.json") as json_file:
+                data = json.load(json_file)
+
+                self.positions = data["positions"]
+                print("Loaded filenames and positions from JSON file.")
+                self.loaded = True
+        else:
+            print("Did not find JSON file, generating metadata...")
+            self.positions = [capture.captures[0].position for capture in itertools.islice(self.dataset.frames(), 1, None, 2)]
+
+        # Save filenames and positions to a JSON file if the save parameter is
+        # set to true.
+        if save and not self.loaded:
+            data = {"positions": self.positions}
+            with open(f"Dataset_metadata{self.captures}.json", "w") as f:
+                json.dump(data, f)
+                print("Saved filenames and positions to JSON file.")
 
         print(f"finished setting up dataset\nSamples in dataset: {self.captures}")
 
     def __getitem__(self, index):
+        position = self.positions[index]
         filename = os.path.join(self.datadir + f"/sequence.{index}", self.img_filename)
-        capture = self.dataset_iterator[index]
-        position = capture.captures[0].position
         image = Image.open(filename).convert("RGB")
 
         if self.transform:
